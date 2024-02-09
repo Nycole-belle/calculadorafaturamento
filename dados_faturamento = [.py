38c90ea7965158fe1e3987a000e9701e0f19dab4
Nycole <nycole.isa@gmail.com>
2024-02-09 18:13:24 -0300

    primeiro commit

---
+++ dados_faturamento = [.py
@@ -0,0 +1,42 @@
+dados_faturamento = [
+    {"dia": 1, "valor": 22174.1664},
+    {"dia": 2, "valor": 24537.6698},
+    {"dia": 3, "valor": 26139.6134},
+    {"dia": 4, "valor": 0.0},
+    {"dia": 5, "valor": 0.0},
+    {"dia": 6, "valor": 26742.6612},
+    {"dia": 7, "valor": 0.0},
+    {"dia": 8, "valor": 42889.2258},
+    {"dia": 9, "valor": 46251.174},
+    {"dia": 10, "valor": 11191.4722},
+    {"dia": 11, "valor": 0.0},
+    {"dia": 12, "valor": 0.0},
+    {"dia": 13, "valor": 3847.4823},
+    {"dia": 14, "valor": 373.7838},
+    {"dia": 15, "valor": 2659.7563},
+    {"dia": 16, "valor": 48924.2448},
+    {"dia": 17, "valor": 18419.2614},
+    {"dia": 18, "valor": 0.0},
+    {"dia": 19, "valor": 0.0},
+    {"dia": 20, "valor": 35240.1826},
+    {"dia": 21, "valor": 43829.1667},
+    {"dia": 22, "valor": 18235.6852},
+    {"dia": 23, "valor": 4355.0662},
+    {"dia": 24, "valor": 13327.1025},
+    {"dia": 25, "valor": 0.0},
+    {"dia": 26, "valor": 0.0},
+    {"dia": 27, "valor": 25681.8318},
+    {"dia": 28, "valor": 1718.1221},
+    {"dia": 29, "valor": 13220.495},
+    {"dia": 30, "valor": 8414.61}
+]
+
+dias_com_faturamento = [dia["valor"] for dia in dados_faturamento if dia["valor"] > 0]
+menor_valor = min(dias_com_faturamento)
+maior_valor = max(dias_com_faturamento)
+media_mensal = sum(dias_com_faturamento) / len(dias_com_faturamento)
+dias_acima_media = sum(1 for dia in dias_com_faturamento if dia > media_mensal)
+
+print(f"O menor valor de faturamento diário é: R$ {menor_valor}")
+print(f"O maior valor de faturamento diário é: R$ {maior_valor}")
+print(f"Número de dias com faturamento acima da média mensal: {dias_acima_media}")
